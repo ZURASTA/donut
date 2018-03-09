7838477d24a97883546707f69c8ab2e5f7d845bd
Stefan Johnson <sj@scrimpycat.io>
2018-03-09 20:18:45 +1100

    Initial setup

---
+++ apps/donut_web/config/test.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :donut_web, Donut.Web.Endpoint,
+    http: [port: 4001],
+    server: false
