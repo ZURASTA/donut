f47e5580f7ec22a17244a355c1e9a105b728d77f
Stefan Johnson <sj@scrimpycat.io>
2018-05-09 22:13:27 +1000

    Add session type

--- apps/donut_graphql/lib/donut.graphql/auth.ex
+++ apps/donut_graphql/lib/donut.graphql/auth.ex
@@ -3,7 +3,13 @@ defmodule Donut.GraphQL.Auth do
 
     @desc "An email authorization credential"
     input_object :email_credential do
-        field :email, non_null(:string), description: "The user email"
+        field :email, non_null(:string), description: "The email for this credential"
         field :password, non_null(:string), description: "The password for this credential"
+    end
+
+    @desc "An identity session"
+    object :session do
+        field :access_token, non_null(:string), description: "An access token use to authenticate requests of an identity"
+        field :refresh_token, non_null(:string), description: "A refresh token used manage the session"
     end
 end
