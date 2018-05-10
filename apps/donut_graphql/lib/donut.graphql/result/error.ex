494f90e0fd4ea3564b7f6ba1740fdc31c33e9fb2
Stefan Johnson <sj@scrimpycat.io>
2018-05-10 23:35:45 +1000

    Add a generic error type

---
+++ apps/donut_graphql/lib/donut.graphql/result/error.ex
@@ -0,0 +1,28 @@
+defmodule Donut.GraphQL.Result.Error do
+    @moduledoc """
+      The base for result errors.
+
+      * `:message` - Contains the presentable error message.
+    """
+    use Absinthe.Schema.Notation
+
+    defstruct [:message]
+
+    @desc "A generic error interface"
+    interface :generic_error do
+        @desc "The presentable error message"
+        field :message, :string
+    end
+
+    @desc "A generic error message"
+    object :error do
+        field :message, :string
+
+        interface :generic_error
+
+        is_type_of fn
+            %Donut.GraphQL.Result.Error{} -> true
+            _ -> false
+        end
+    end
+end
