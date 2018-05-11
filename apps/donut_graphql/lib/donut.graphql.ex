77f98d850d6ff160c7e2e1e7d18c1ee81949bff7
Stefan Johnson <sj@scrimpycat.io>
2018-05-12 02:32:29 +1000

    Expose auth mutations and add debug directive

--- apps/donut_graphql/lib/donut.graphql.ex
+++ apps/donut_graphql/lib/donut.graphql.ex
@@ -1,6 +1,17 @@
 defmodule Donut.GraphQL do
     use Absinthe.Schema
+    import_types Donut.GraphQL.Result.Error
+    import_types Donut.GraphQL.Result.InternalError
+    import_types Donut.GraphQL.Auth
 
     query do
+    end
+
+    mutation do
+        import_fields :auth_mutations
+    end
+
+    directive :debug do
+        on [:field]
     end
 end
