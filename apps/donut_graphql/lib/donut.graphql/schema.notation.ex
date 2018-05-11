b6209131ac5476cd1204d5ac01f3a6ca70f63ab7
Stefan Johnson <sj@scrimpycat.io>
2018-05-12 02:26:35 +1000

    Add docs and specs

--- apps/donut_graphql/lib/donut.graphql/schema.notation.ex
+++ apps/donut_graphql/lib/donut.graphql/schema.notation.ex
@@ -1,4 +1,8 @@
 defmodule Donut.GraphQL.Schema.Notation do
+    @moduledoc """
+      Sets up the notations for building an Absinthe schema.
+    """
+
     defmacro __using__(_options) do
         quote do
             use Absinthe.Schema.Notation, except: [resolve: 1]
@@ -9,16 +13,26 @@ defmodule Donut.GraphQL.Schema.Notation do
 
     require Logger
 
+    @type parent :: map
+    @type args :: map
+    @type env :: Absinthe.Resolution.t
+    @type result :: { :ok, any } | { :error, any }
+    @type resolver :: (args, env -> result) | (parent, args, env -> result)
+
+    @doc false
+    @spec resolve(resolver) :: Macro.t
     defmacro resolve(fun) do
         quote do
             Absinthe.Schema.Notation.resolve(&Donut.GraphQL.Schema.Notation.run(&1, &2, &3, unquote(fun)))
         end
     end
 
+    @spec resolver(parent, args, env, resolver) :: result
     defp resolver(_, args, env, fun) when is_function(fun, 2), do: fun.(args, env)
     defp resolver(parent, args, env, fun), do: fun.(parent, args, env)
 
     @doc false
+    @spec run(parent, args, env, resolver) :: result
     def run(parent, args, env = %{ definition: %{ directives: directives } }, fun) do
         if Enum.any?(directives, fn
             %{ schema_node: %{ identifier: :debug } } -> true
