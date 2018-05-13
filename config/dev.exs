f9fb60432d3b9647674e12ca36349df6ff407254
Stefan Johnson <sj@scrimpycat.io>
2018-05-13 22:35:55 +1000

    Setup docs and generate GraphQL docs

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,19 @@
 use Mix.Config
 
+import_config "simple_markdown_rules.exs"
+
+config :simple_markdown_extension_highlight_js,
+    source: Enum.at(Path.wildcard(Path.join(Mix.Project.deps_path(), "ex_doc/formatters/html/dist/*.js")), 0, "")
+
+config :ex_doc_simple_markdown, [
+    rules: fn rules ->
+        :ok = SimpleMarkdownExtensionHighlightJS.setup
+        SimpleMarkdownExtensionBlueprint.add_rule(rules)
+    end
+]
+
+config :ex_doc, :markdown_processor, ExDocSimpleMarkdown
+
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
--- mix.exs
+++ mix.exs
@@ -5,7 +5,17 @@ defmodule Donut.Mixfile do
         [
             apps_path: "apps",
             start_permanent: Mix.env == :prod,
-            deps: deps()
+            aliases: aliases(),
+            deps: deps(),
+            dialyzer: [plt_add_deps: :transitive],
+            name: "Donut",
+            source_url: "https://github.com/ZURASTA/donut",
+            docs: [
+                main: "donut",
+                extras: [
+                    "README.md": [filename: "donut", title: "Donut"]
+                ]
+            ]
         ]
     end
 
@@ -22,6 +32,38 @@ defmodule Donut.Mixfile do
     # Dependencies listed here are available only for this project
     # and cannot be accessed from applications inside the apps folder
     defp deps do
-        []
+        [
+            { :ex_doc, "~> 0.18", only: :dev, runtime: false },
+            { :simple_markdown, "~> 0.5.2", only: :dev, runtime: false },
+            { :ex_doc_simple_markdown, "~> 0.2.1", only: :dev, runtime: false },
+            { :simple_markdown_extension_blueprint, "~> 0.2", only: :dev, runtime: false },
+            { :simple_markdown_extension_highlight_js, "~> 0.1.0", only: :dev, runtime: false },
+            { :blueprint, "~> 0.3.1", only: :dev, runtime: false }
+        ]
+    end
+
+    defp aliases do
+        [docs: &build_docs/1]
+    end
+
+    defp build_docs(_) do
+        System.cmd("mix", ["compile"], env: [{ "MIX_ENV", "prod" }])
+
+        Mix.Tasks.Compile.run([])
+        Mix.Tasks.Docs.run([])
+
+
+        Application.put_env(:phoenix, :serve_endpoints, true, persistent: true)
+        Application.ensure_all_started(:donut_web)
+
+        { schema, 0 } = System.cmd("get-graphql-schema", [Donut.Web.Endpoint.static_url])
+        File.write!("donut.graphql", schema)
+
+        { _, 0 } = System.cmd("ruby", ["-rgraphql-docs", "-e", "GraphQLDocs.build filename: 'donut.graphql', base_url: '.', output_dir: 'doc/api'"])
+
+        IO.puts "#{IO.ANSI.green}GraphQL API docs successfully generated#{IO.ANSI.reset}"
+        IO.puts "#{IO.ANSI.green}View them at \"doc/api/index.html\".#{IO.ANSI.reset}"
+
+        File.rm!("donut.graphql")
     end
 end
