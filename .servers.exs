82d3f1d0a1a1bb85f47f9b77d6cea5533f3b9ae2
Stefan Johnson <sj@scrimpycat.io>
2018-05-30 01:16:33 +1000

    Remove uneeded match

--- .servers.exs
+++ .servers.exs
@@ -1,3 +1,1 @@
-[
-    { { :call, _, { :call, _, { :remote, _, { :atom, _, Access }, { :atom, _, :get } }, [{ :var, _, _ }, { :atom, _, :server }, { :atom, _, _ }] }, [{ :atom, _, name }] }, name }
-]
+[]
