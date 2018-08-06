8fbcfe6c760daeda9e297291b5df1f2eb5fff8b1
Stefan Johnson <sj@scrimpycat.io>
2018-08-06 21:12:39 +1000

    Document capture middleware

--- apps/donut_graphql/lib/donut.graphql/middleware/capture.ex
+++ apps/donut_graphql/lib/donut.graphql/middleware/capture.ex
@@ -1,4 +1,21 @@
 defmodule Donut.GraphQL.Middleware.Capture do
+    @moduledoc """
+      An Absinthe middleware to capture fields from resolves result and place
+      them into the context.
+
+      A capture can take the form of `atom | { context_key :: atom, result_key :: atom }`
+      and can be a list of captures to capture multiple values. Captured values
+      will overwrite any previous values with the same name in the context.
+
+        field :foo, type:  do
+            resolve fn _, _ -> { :ok, %{ a: 1, b: 2, c: 3 } }
+            end
+
+            middleware Donut.GraphQL.Middleware.Capture, :a \# Adds the value for :a to the context
+            middleware Donut.GraphQL.Middleware.Capture foo: :b \# Adds the value for :b to the context with name :foo
+      end
+    """
+
     @behaviour Absinthe.Middleware
 
     @impl Absinthe.Middleware
